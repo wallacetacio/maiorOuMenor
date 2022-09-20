c6c26374a326feb521d4f640e39ab9c842dbc4b1
Wallace Tácio <wallacett.dev@gmail.com>
2022-09-20 10:54:44 -0300

    Adiciona solução ao exercício Maior ou Menor FATEC 2022/2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿double num;
+
+Console.Write("Digite um número: ");
+
+num = Convert.ToDouble(Console.ReadLine());
+
+if(num > 20){
+    Console.WriteLine($"Número maior que 20 portanto sua metade é: {num/2}");
+    Console.ReadKey();
+}else{
+    Console.WriteLine($"Número menor ou igual a 20 portanto seu dobro é: {num*2}");
+}
+Console.ReadKey();
